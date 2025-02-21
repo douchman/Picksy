04b217ca7dcca19dcbce143a8ff0ce577af7fa96
buck <take6948@naver.com>
2025-02-21 17:03:51 +0900

    :tada: 프로젝트 초기화

---
+++ src/main/java/com/buck/vstournament/VstournamentApplication.java
@@ -0,0 +1,13 @@
+package com.buck.vstournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VstournamentApplication.class, args);
+	}
+
+}
