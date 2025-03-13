02e9a503cc603eaa453849836f62f80d48408c3a
buck <take6948@naver.com>
2025-03-13 15:40:32 +0900

    :sparkles: 계정생성 api 연결 #62 - 가입 form 유효검사 함수 수정

--- src/main/resources/static/member/register/js/register.js
+++ src/main/resources/static/member/register/js/register.js
@@ -7,12 +7,13 @@ import {
     passwordValidationMessage,
     isPasswordEqual, passwordMismatchMessage
 } from "../../login/js/validation.js";
+import {apiPostRequest} from "../../../global/js/api.js";
 
 
 document.addEventListener('DOMContentLoaded', async () => {
 
-    document.querySelector('#btn-register').addEventListener('click', () => {
-        registerMember();
+    document.querySelector('#btn-register').addEventListener('click', async () => {
+        await registerMember();
     });
 
     document.querySelector('#btn-back-to-login').addEventListener('click', () => {
@@ -20,9 +21,17 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
 });
 
-function registerMember(){
-    if(validateRegisterForm()){
-        // register available
+async function registerMember(){
+    const { validationResult , form : requestBody } = validateRegisterForm();
+    if( validationResult ){
+        const {status, data } = await postMember(requestBody);
+
+        if ( status !== 200 ){
+            showRegisterMessage(data.message);
+        } else {
+            alert('가입이 완료되었습니다.') // 임시
+            location.href = '/login';
+        }
     }
 }
 
@@ -34,29 +43,39 @@ function validateRegisterForm(){
 
     if(!validateId(registerId)){
         showRegisterMessage(idValidationMessage);
-        return false;
+        return {validationResult : false, form :{} };
     }
 
     else if(!validateName(registerName)){
         showRegisterMessage(nameValidationMessage);
-        return false;
+        return {validationResult : false, form :{} };
     }
 
     else if(!isPasswordEqual(password, passwordConfirm)){
         showRegisterMessage(passwordMismatchMessage);
-        return false;
+        return {validationResult : false, form :{} };
     }
 
     else if(!validatePassword(password, passwordConfirm)){
         showRegisterMessage(passwordValidationMessage);
-        return false;
+        return {validationResult : false, form :{} };
     }
 
-    return true;
+    return {
+        validationResult : true,
+        form :{
+            loginId : registerId,
+            memberName : registerName,
+            password : password
+        } };
 }
 
 function showRegisterMessage(message){
     const messageBox = document.querySelector('#message-box');
     messageBox.textContent = message;
     messageBox.classList.add('show')
+}
+
+async function postMember(requestBody){
+    return await apiPostRequest('member', {}, requestBody);
 }
