006e853bdcc568982f32c1956e9c45e14f27a8a8
buck <take6948@naver.com>
2025-03-14 15:52:21 +0900

    :recycle: 비밀번호 정규식 개선 #62

--- src/main/resources/static/member/login/js/validation.js
+++ src/main/resources/static/member/login/js/validation.js
@@ -1,6 +1,6 @@
 const idRegex = /^[a-z0-9]{6,20}$/;
 const nameRegex = /^[a-zA-Z0-9가-힣]{1,12}$/;
-const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+~={}\[\]:;"'<>,.?/])[a-zA-Z\d!@#$%^&*()_+~={}\[\]:;"'<>,.?/]{8,20}$/;
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*\W).{8,20}$/;
 
 export const idValidationMessage = '아이디는 영어(소문자), 숫자 조합 6 ~ 20자 까지 가능합니다.'
 export const nameValidationMessage = '이름은 한글, 영어(대/소문자), 숫자 조합 최대 12자까지 가능합니다. '
