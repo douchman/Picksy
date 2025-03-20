30621b1c97130bc6bb9b7a3cf9512dde02004892
buck <take6948@naver.com>
2025-03-20 14:24:01 +0900

    :sparkles: 대결주제 설정 영역 파일 등록 기능 구현 #85

--- src/main/resources/static/topic/create/js/topic-create.js
+++ src/main/resources/static/topic/create/js/topic-create.js
@@ -1,3 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+import {addTopicCreateEvents} from "./l-topic-create.js";
 
+document.addEventListener('DOMContentLoaded', () => {
+    addTopicCreateEvents();
 });
