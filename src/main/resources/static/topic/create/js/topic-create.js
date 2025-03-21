9ea0e7a3c4d06da2e9099961f6867f4fb61c99eb
buck <take6948@naver.com>
2025-03-21 18:19:56 +0900

    :sparkles: 대결 주제 생성 메인 JS 추가 #86

--- src/main/resources/static/topic/create/js/topic-create.js
+++ src/main/resources/static/topic/create/js/topic-create.js
@@ -1,7 +1,23 @@
-import {addTopicCreateEvents} from "./l-topic-create.js";
-import {addEntryCreateEvents} from "./r-entry-create.js";
+import {addTopicCreateEvents, registerTopic} from "./l-topic-create.js";
+import {addEntryCreateEvents, registerEntries} from "./r-entry-create.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     addTopicCreateEvents();
     addEntryCreateEvents();
+    addTopicCrateEvents();
 });
+
+
+function addTopicCrateEvents(){
+    document.querySelector('#btn-save').addEventListener('click' , async () => {
+
+        registerTopic().then(() =>{
+            registerEntries();
+        })
+
+    });
+
+    document.querySelector('#btn-cancel').addEventListener('click' , async () => {
+        location.href = '/';
+    });
+}
