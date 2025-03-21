ba94b1e8eb27d42c8257e226ffc31353187fb775
buck <take6948@naver.com>
2025-03-21 14:16:27 +0900

    :sparkles: 엔트리 등록 영역 드래그 기능 추가 #85

--- src/main/resources/static/topic/create/js/r-entry-create.js
+++ src/main/resources/static/topic/create/js/r-entry-create.js
@@ -1,7 +1,39 @@
+import {generateFilePreviewURL} from "../../../global/js/file.js";
+
 export function addEntryCreateEvents(){
 
     document.querySelector('#add-entry').addEventListener('click', function(){
         renderEmptyEntryItem();
+    });
+    document.querySelector('#add-entry').addEventListener('dragover', function(e){
+        e.preventDefault();
+        this.classList.add('drag-over');
+    });
+    document.querySelector('#add-entry').addEventListener('dragleave', function(e){
+        e.preventDefault();
+        this.classList.remove('drag-over');
+    });
+
+    document.querySelector('#add-entry').addEventListener('drop', function(e){
+        e.preventDefault();
+        const dataTransfer = new DataTransfer();
+        const files = e.dataTransfer.files;
+        const entryFile = document.querySelector('#entry-image');
+
+        for(const file of files){
+            dataTransfer.items.add(file);
+        }
+
+        entryFile.files = dataTransfer.files;
+
+        requestAnimationFrame(() => {
+            Array.from(entryFile.files).forEach(file => {
+                generateFilePreviewURL(file, (url) =>{
+                    renderEmptyEntryItem(url);
+                })
+            });
+        });
+        this.classList.remove('drag-over');
     });
 
     document.querySelector('#entry-form').addEventListener('click', function(event){
@@ -21,11 +53,11 @@ export function addEntryCreateEvents(){
 
 }
 
-function renderEmptyEntryItem(){
+function renderEmptyEntryItem(thumbnail){
     const entryForm = document.querySelector('#entry-form');
     const entryItem =
         `<div class="entry-item">
-            <div class="entry-thumb"></div>
+            <div class="entry-thumb" style="background-image: url(${thumbnail})"></div>
                 <div class="entry-desc">
                     <div class="entry-desc-input-group">
                         <span class="input-index">엔트리 명</span>
