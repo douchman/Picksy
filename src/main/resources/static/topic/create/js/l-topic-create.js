fb268bf6fe174a31db42d68f58962759e1f08637
buck <take6948@naver.com>
2025-03-21 17:39:51 +0900

    :sparkles: 대결주제 등록 API 연결 및 기능 구현 #86

--- src/main/resources/static/topic/create/js/l-topic-create.js
+++ src/main/resources/static/topic/create/js/l-topic-create.js
@@ -1,4 +1,6 @@
 import {generateFilePreviewURL} from "../../../global/js/file.js";
+import {apiFormDataRequest} from "../../../global/js/api.js";
+import {setTopicId} from "./const.js";
 
 export function addTopicCreateEvents(){
 
@@ -41,4 +43,69 @@ export function addTopicCreateEvents(){
         });
         this.classList.remove('drag-over');
     });
+}
+
+export async function registerTopic(){
+    const {validationResult , formData : {topicTitle, topicSubject, topicDesc, topicThumb, visibility }} = validateAndGenerateTopicFormData();
+
+    if( validationResult ){
+        const requestBody = new FormData();
+        requestBody.append('title', topicTitle);
+        requestBody.append('subject', topicSubject);
+        requestBody.append('description', topicDesc);
+        requestBody.append('thumbnail', topicThumb);
+        requestBody.append('visibility', visibility);
+
+        const {status, data : registerResult } = await postTopic(requestBody);
+
+        if( status === 200){
+            setTopicId(registerResult.topicId);
+        }
+
+    }
+
+}
+
+function validateAndGenerateTopicFormData(){
+    const topicTitle = document.querySelector('#topic-title').value;
+    const topicSubject = document.querySelector('#topic-subject').value;
+    const topicDesc = document.querySelector('#topic-desc').value;
+    const topicThumb = document.querySelector('#topic-thumbnail').files[0];
+    const visibility = document.querySelector('input[name="visibility"]:checked')?.value;
+
+    if(!topicTitle || topicTitle === ''){
+        alert('대결 제목을 입력해주세요'); // 임시
+        return {validationResult : false, formData : {}}
+    }
+
+    if(!topicSubject || topicSubject === ''){
+        alert('주요개념을 입력해주세요'); // 임시
+        return {validationResult : false, formData : {}}
+    }
+
+    if(!topicDesc || topicDesc === ''){
+        alert('대결 설명을 입력해주세요'); // 임시
+        return {validationResult : false, formData : {}}
+    }
+
+    if(!topicThumb){
+        alert('대표이미지를 등록해주세요.'); // 임시
+        return {validationResult : false, formData : {}}
+    }
+
+    return {
+        validationResult : true,
+        formData:  {
+            topicTitle : topicTitle,
+            topicSubject : topicSubject,
+            topicDesc : topicDesc,
+            topicThumb : topicThumb,
+            visibility :visibility
+
+        }
+    };
+}
+
+async function postTopic(requestBody){
+    return apiFormDataRequest('topics', {}, requestBody);
 }
