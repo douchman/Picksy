e8b9f7354ae797c8c64e84ee2400d216de542a1a
buck <take6948@naver.com>
2025-03-27 16:51:34 +0900

    :sparkles: 페이지 내 전역으로 사용 할 대결주제 및 대결진행정보 객체 추가 #110

---
+++ src/main/resources/static/topic/play/js/playRecord.js
@@ -0,0 +1,13 @@
+const playRecord = {
+    id : null,
+
+    setId(id) {
+        this.id = id;
+    },
+
+    getId() {
+        return this.id
+    }
+}
+
+export default playRecord;
---
+++ src/main/resources/static/topic/play/js/topic.js
@@ -0,0 +1,13 @@
+const topic = {
+    id : null,
+
+    setId(id) {
+        this.id = id;
+    },
+
+    getId() {
+        return this.id
+    }
+}
+
+export default topic;
