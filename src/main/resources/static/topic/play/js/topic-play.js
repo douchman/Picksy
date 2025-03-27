6dca70a6b1d1d8dd291863de84960f5c17d22990
buck <take6948@naver.com>
2025-03-27 16:52:30 +0900

    :sparkles: 페이지 진입 후 대결주제 식별자 및 진행정보 식별자 변수로 저장 #110

--- src/main/resources/static/topic/play/js/topic-play.js
+++ src/main/resources/static/topic/play/js/topic-play.js
@@ -1,3 +1,31 @@
-document.addEventListener('DOMContentLoaded', () => {
+import topic from "./topic.js";
+import playRecord from "./playRecord.js";
+import {showToastMessage} from "../../../global/popup/js/common-toast-message";
 
+document.addEventListener('DOMContentLoaded', async () => {
+    setTopic();
+    setPlayRecordId();
 });
+
+function setTopic(){
+    const path = window.location.pathname;
+    const segments = path.split('/');
+    const topicId = segments[segments.length - 1];
+
+    topic.setId(topicId);
+}
+
+function setPlayRecordId(){
+    const storedPlayRecordIdName = `topic-${topic.getId()}-playRecord-id`;
+    try {
+        const playRecordId = localStorage.getItem(storedPlayRecordIdName);
+        playRecord.setId(playRecordId);
+    } catch(error) {
+        showToastMessage('대결 진행 정보를 확인할 수 없어요 :(' , 'error', 3500);
+        setTimeout(()=> {
+            location.href = '/';
+        }, 2500);
+    } finally { // 처리 후 스토리지는 제거
+        localStorage.removeItem(storedPlayRecordIdName);
+    }
+}
