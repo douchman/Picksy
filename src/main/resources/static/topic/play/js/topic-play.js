2ffbedb4393085efe4b2fd7b4ab7e7f1f94c337d
buck <take6948@naver.com>
2025-03-28 15:17:12 +0900

    :recycle: 대결 주제 상세 정보 조회 & 저장 #110

--- src/main/resources/static/topic/play/js/topic-play.js
+++ src/main/resources/static/topic/play/js/topic-play.js
@@ -1,31 +1,67 @@
-import topic from "./topic.js";
-import playRecord from "./playRecord.js";
-import {showToastMessage} from "../../../global/popup/js/common-toast-message";
+import {topic, playRecord} from "./const.js";
+import {showToastMessage} from "../../../global/popup/js/common-toast-message.js";
+import {loadEntryMatchInfo} from "./playRecord.js";
+import {apiGetRequest} from "../../../global/js/api";
 
 document.addEventListener('DOMContentLoaded', async () => {
-    setTopic();
-    setPlayRecordId();
+    const topicSuccess  = await setTopic(); // 대결주제 식별자 변수화
+    const playRecordSuccess = setPlayRecordId(); // 대결진행기록 식별자 변수화
+
+    if( topicSuccess && playRecordSuccess){
+        await loadEntryMatchInfo();
+    }
 });
 
-function setTopic(){
+async function setTopic(){
     const path = window.location.pathname;
     const segments = path.split('/');
     const topicId = segments[segments.length - 1];
 
     topic.setId(topicId);
+
+    const { status, data : topicDetail } = await getTopicDetail();
+    if ( status === 200){
+        topic.setTitle(topicDetail.topic.title);
+    } else {
+        handleSetTopicFailed();
+    }
+
+    return true;
 }
 
 function setPlayRecordId(){
     const storedPlayRecordIdName = `topic-${topic.getId()}-playRecord-id`;
     try {
         const playRecordId = localStorage.getItem(storedPlayRecordIdName);
-        playRecord.setId(playRecordId);
+
+        if( !playRecordId ){
+            handleSetPlayRecordIdFailed();
+        } else {
+            playRecord.setId(playRecordId);
+        }
     } catch(error) {
-        showToastMessage('대결 진행 정보를 확인할 수 없어요 :(' , 'error', 3500);
-        setTimeout(()=> {
-            location.href = '/';
-        }, 2500);
+        handleSetPlayRecordIdFailed();
     } finally { // 처리 후 스토리지는 제거
         localStorage.removeItem(storedPlayRecordIdName);
     }
+
+    return true;
+}
+
+async function getTopicDetail(){
+    return await apiGetRequest(`topics/${topic.getId()}`);
+}
+
+function handleSetPlayRecordIdFailed(){
+    showToastMessage('대결 진행 정보를 확인할 수 없어요 :(' , 'error', 3500);
+    setTimeout(()=> {
+        location.href = '/';
+    }, 2500);
+}
+
+function handleSetTopicFailed(){
+    showToastMessage('대결 정보를 확인할 수 없어요 :(' , 'error', 3500);
+    setTimeout(()=> {
+        location.href = '/';
+    }, 2500);
 }
