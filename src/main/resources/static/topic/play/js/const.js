bed173eccfbc53f313886effe732985f88f4baa2
buck <take6948@naver.com>
2025-03-28 15:15:28 +0900

    :recycle: 대결 주제 및 진행정보 객체 통합 #110

---
+++ src/main/resources/static/topic/play/js/const.js
@@ -0,0 +1,32 @@
+export const topic = {
+    id : null,
+    title : null,
+
+    setId(id) {
+        this.id = id;
+    },
+
+    getId() {
+        return this.id
+    },
+
+    setTitle(title) {
+        this.title = title
+    },
+
+    getTitle(){
+        return this.title;
+    }
+}
+
+export const playRecord = {
+    id : null,
+
+    setId(id) {
+        this.id = id;
+    },
+
+    getId() {
+        return this.id
+    }
+}
--- src/main/resources/static/topic/play/js/playRecord.js
+++ src/main/resources/static/topic/play/js/playRecord.js
@@ -1,13 +1,23 @@
-const playRecord = {
-    id : null,
+import {apiGetRequest} from "../../../global/js/api.js";
+import {playRecord, topic} from "./const.js";
+import {showToastMessage} from "../../../global/popup/js/common-toast-message.js";
 
-    setId(id) {
-        this.id = id;
-    },
+export async function loadEntryMatchInfo() {
+    const {status, data: matchInfo} = await getMatch();
 
-    getId() {
-        return this.id
+    if (status === 200) {
+
+        const topicTitle = topic - title
+
+    } else {
+        showToastMessage('대결 정보 조회 중 문제가 생겼어요<br> 잠시 후 다시 시도해주세요', 'alert', 3000);
+        setTimeout(() => {
+            location.href = '/';
+        }, 2000);
     }
 }
 
-export default playRecord;
+
+async function getMatch(){
+    return await apiGetRequest(`topics/play-records/${playRecord.getId()}/matches`);
+}
