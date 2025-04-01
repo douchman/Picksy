dca8237b7a5be40c6af86eddd92da9f58f040d61
buck <take6948@naver.com>
2025-04-01 16:47:08 +0900

    :memo: 함수 주석 추가 #110

--- src/main/resources/static/topic/play/js/entry-match.js
+++ src/main/resources/static/topic/play/js/entry-match.js
@@ -1,10 +1,12 @@
 import {apiGetRequest, apiPatchRequest} from "../../../global/js/api.js";
 import {match, playRecord} from "./const.js";
 
+// 엔트리 대진표 조회
 export async function getMatch(){
     return await apiGetRequest(`topics/play-records/${playRecord.getId()}/matches`);
 }
 
+// 엔트리 대결 결과 제출
 export async function submitEntryMatchResult(winnerEntryId, loserEntryId){
     const requestBody = {
         winnerEntryId : winnerEntryId,
