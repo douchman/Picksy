f1346e5c38239cfc1a44503c7a796af121e11cbf
buck <take6948@naver.com>
2025-04-01 18:51:23 +0900

    :sparkles: 브라우저 localStorage 에 저장된 대결진행 식별자 비우기 #113

--- src/main/resources/static/home/js/home.js
+++ src/main/resources/static/home/js/home.js
@@ -2,6 +2,7 @@ import {apiGetRequest} from '../../global/js/api.js';
 import {addDialogEvents, renderDialog, openTournamentSelectDialog} from "./tournament-select-dialog.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
+    flushPlayRecordIdsFromLocalStorage();
     await renderTopics();
     addTopicCardEvents();
     renderDialog();
@@ -56,4 +57,22 @@ async function renderTopics(){
 function clearTopicContentCards(){
     const topicContentCards = document.querySelector('#topic-content-cards');
     topicContentCards.replaceChildren();
+}
+
+
+
+/**
+ * 로컬스토리지에 저장된 대결진행관련 식별자 모두 비우기
+ */
+function flushPlayRecordIdsFromLocalStorage(){
+    const keysToDelete = [];
+
+    for( let i = 0; i< localStorage.length; i++){
+        const key = localStorage.key(i);
+        if( key && key.startsWith('topic-') && key.endsWith('-playRecord-id')){
+            keysToDelete.push(key);
+        }
+    }
+
+    keysToDelete.forEach( key => localStorage.removeItem(key));
 }
