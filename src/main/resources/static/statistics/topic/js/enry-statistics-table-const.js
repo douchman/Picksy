a5dd4de8d4477ec029f0feeb823470111de2c932
buck <take6948@naver.com>
2025-04-11 15:31:07 +0900

    :sparkles: 테이블관련 필터 및 정렬 기준 query const 추가 #135

---
+++ src/main/resources/static/statistics/topic/js/enry-statistics-table-const.js
@@ -0,0 +1,43 @@
+const OrderType = {
+    ASC: 'ASC',
+    DESC: 'DESC',
+};
+
+const tableQuery = {
+    currentPage : 1,
+    pageSize : 10,
+    rankOrder : OrderType.ASC,
+    totalMatchesOrder : OrderType.DESC,
+    totalWinsOrder : OrderType.DESC,
+    winRateOrder : OrderType.DESC,
+
+
+    setCurrentPage(currentPage){
+        this.currentPage = currentPage;
+    },
+
+    getCurrentPage(){
+        return this.currentPage
+    },
+
+    setPageSize(pageSize){
+        this.pageSize = pageSize;
+    },
+
+    getPageSize(){
+        return this.pageSize;
+    },
+
+    setRankOrder(orderType){
+        this.rankOrder = orderType;
+    },
+    setTotalMatchesOrder(orderType){
+        this.totalMatchesOrder = orderType;
+    },
+    setTotalWinsOrder(orderType){
+        this.totalWinsOrder = orderType;
+    },
+    setWinRateOrder(orderType){
+        this.winRateOrder = orderType;
+    },
+}
