20fa0cbd6c978eb753d9a01e67f3c7ce4dddc3c0
buck <take6948@naver.com>
2025-04-11 17:58:31 +0900

    :bug: 기본 표기갯수를 10으로 조정 #137

--- src/main/resources/static/statistics/topic/js/enry-statistics-table-const.js
+++ src/main/resources/static/statistics/topic/js/enry-statistics-table-const.js
@@ -7,7 +7,7 @@ export const tableQuery = {
     totalItems : 0,
     totalPages : 0,
     currentPage : 1,
-    pageSize : 1,
+    pageSize : 10,
     rankOrder : OrderType.ASC,
     totalMatchesOrder : OrderType.DESC,
     totalWinsOrder : OrderType.DESC,
