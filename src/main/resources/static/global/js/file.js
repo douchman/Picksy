622dbc43de6da7516cdc8c45a10492aea36d44e0
buck <take6948@naver.com>
2025-04-14 19:26:15 +0900

    :sparkles: 등록된 비디오 파일로부터 이미지 추출 #152

--- src/main/resources/static/global/js/file.js
+++ src/main/resources/static/global/js/file.js
@@ -1,3 +1,4 @@
+// 이미지 유형 미리보기
 export function generateFilePreviewURL(file, callback){
     if(!file) {
        return;
@@ -11,4 +12,55 @@ export function generateFilePreviewURL(file, callback){
 
         reader.readAsDataURL(file);
     }
+}
+
+// 비디오 유형 미리보기
+export function generateVideoPreviewRL(file, callback){
+    if(!file){
+        return;
+    }
+
+    if( file ){
+        getThumbnailForVideo(file).then((imageUrl) =>{
+            callback(imageUrl);
+        });
+    }
+}
+
+// 비디오 파일로부터 미리보기 이미지 생성
+async function getThumbnailForVideo(videoFile) {
+    const videoUrl = URL.createObjectURL(videoFile);
+    const video = document.createElement("video");
+    const canvas = document.createElement("canvas");
+    video.style.display = "none";
+    canvas.style.display = "none";
+
+    // Trigger video load
+    await new Promise((resolve, reject) => {
+        video.addEventListener("loadedmetadata", () => {
+            video.width = video.videoWidth;
+            video.height = video.videoHeight;
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            // Seek the video to 25%
+            video.currentTime = video.duration * 0.25;
+        });
+        video.addEventListener("seeked", () => resolve());
+        video.src = videoUrl;
+    });
+
+    // Draw the thumbnail
+    canvas
+        .getContext("2d")
+        .drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    return canvas.toDataURL("image/png");
+}
+
+// 생성된 미리보기 이미지를 파일로 생성
+export function getThumbBlobFromVideoUrl(videoUrl) {
+    const [meta, base64] = videoUrl.split(',');
+    const mime = meta.match(/:(.*?);/)[1];
+    const binary = atob(base64);
+    const array = Uint8Array.from(binary, c => c.charCodeAt(0));
+    return new Blob([array], { type: mime });
 }
