a5008655339144f3d75b74f092912458025fbb32
buck <take6948@naver.com>
2025-04-15 17:36:53 +0900

    :sparkles: 유튜브 URL 로부터 비디오 식별자 추출 함수 추가 #158

--- src/main/resources/static/global/js/youtube-iframe-api.js
+++ src/main/resources/static/global/js/youtube-iframe-api.js
@@ -19,3 +19,10 @@ export function onYouTubeIframeApiReady(callBack) {
         }
     };
 }
+
+// 유튜브 URL 로 부터 비디오 ID 추출
+export function extractYoutubeVideoIdFromUrl(url) {
+    const regExp = /(?:youtube\.com\/.*[?&]v=|youtu\.be\/|youtube\.com\/embed\/)([^&?/]+)/;
+    const match = url.match(regExp);
+    return match ? match[1] : null;
+}
