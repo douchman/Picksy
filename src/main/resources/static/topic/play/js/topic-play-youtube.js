e79ea4289a962dba3587ad8820768aeee95900c9
buck <take6948@naver.com>
2025-04-15 17:37:40 +0900

    :fire: 불필요한 소스코드 제거 #158

--- src/main/resources/static/topic/play/js/topic-play-youtube.js
+++
@@ -1,5 +0,0 @@
-export function extractYoutubeVideoIdFromUrl(url) {
-    const regExp = /(?:youtube\.com\/.*[?&]v=|youtu\.be\/|youtube\.com\/embed\/)([^&?/]+)/;
-    const match = url.match(regExp);
-    return match ? match[1] : null;
-}
