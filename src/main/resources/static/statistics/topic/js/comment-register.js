2449e71cc099a433184a286d97729d7c24d5b7f8
buck <take6948@naver.com>
2025-04-18 14:28:46 +0900

    :sparkles: 댓글 작성 기능 추가 #165

---
+++ src/main/resources/static/statistics/topic/js/comment-register.js
@@ -0,0 +1,75 @@
+import {apiPostRequest} from "../../../global/js/api.js";
+import {topic} from "./const.js";
+import {showToastMessage} from "../../../global/popup/js/common-toast-message.js";
+
+// 댓글 작성 이벤트
+export function addCommentRegisterEvent(){
+    document.querySelector('#btn-register-comment').addEventListener('click', async function(){
+        await registerComment();
+    });
+}
+
+// 댓글 작성
+async function registerComment(){
+    toggleRegisterButtonBlock(true);
+
+    const registerRequestBody = {
+        author : document.querySelector('#author').value,
+        content : document.querySelector('#comment-content').value,
+    }
+
+    const {status, data : registerResult } = await postComments(registerRequestBody);
+
+    if( status === 200 ){
+        initCommentRegisterForm(); // 코멘트 form 초기화
+        commentListScrollTop(); // 스크롤 최상단
+        setTimeout(() => {
+            renderRegisteredComment(registerResult.author, registerResult.content, registerResult.createdAt);
+        }, 300); // 지연 후 랜더링
+
+    } else{
+        showToastMessage('댓글 작성에 문제가 발생했어요. 잠시 후 다시 시도해 주세요');
+    }
+    toggleRegisterButtonBlock(false);
+}
+
+// 작성된 댓글 랜더링
+function renderRegisteredComment(author, content, createdAt){
+    const commentList = document.querySelector('#comment-list');
+    const registeredComment =
+        `<div class="comment blink">
+            <div class="comment-header">
+                <p class="author">${author}</p>
+                <span class="time-stamp">${createdAt}</span>
+                <button class="btn-report" type="button"></button>
+            </div>
+            <p class="content">${content}</p>
+        </div>`;
+
+    commentList.insertAdjacentHTML('afterbegin', registeredComment);
+    commentList.classList.remove('empty');
+}
+
+// 코멘트 남기기 버튼 disable 제어
+function toggleRegisterButtonBlock(isBlock = false){
+    const btnRegisterComment = document.querySelector('#btn-register-comment');
+    isBlock ?
+        btnRegisterComment.disabled = true
+        : btnRegisterComment.disabled = false
+}
+
+// 댓글 창 스크롤 최상단으로 이동
+function commentListScrollTop(){
+    document.querySelector('#comment-list-wrapper').scrollTop = 0;
+}
+
+// 댓글 작성 창 초기화
+function initCommentRegisterForm(){
+    document.querySelector('#author').value = '익명';
+    document.querySelector('#comment-content').value = '';
+}
+
+// 댓글 작성 api
+async function postComments(requestBody){
+    return await apiPostRequest(`topics/${topic.getId()}/comments`, {}, requestBody)
+}
