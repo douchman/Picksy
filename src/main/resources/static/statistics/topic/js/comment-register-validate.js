7814d95668d83373fbea289508991077b4c4e22a
buck <take6948@naver.com>
2025-04-22 15:59:18 +0900

    :recycle: 한글 초성도 입력이 가능하도록 정규식 조정 #171

--- src/main/resources/static/statistics/topic/js/comment-register-validate.js
+++ src/main/resources/static/statistics/topic/js/comment-register-validate.js
@@ -1,5 +1,5 @@
-const authorRegex = /^[가-힣a-zA-Z0-9]{2,255}$/;
-const commentContentRegex = /^[가-힣a-zA-Z0-9\s.,!?()@#%&*~_\-+=:;]{2,500}$/;
+const authorRegex = /^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9]{2,255}$/;
+const commentContentRegex = /^[가-힣ㄱ-ㅎㅏ-ㅣa-zA-Z0-9\s.,!?()@#%&*~_\-+=:;]{2,500}$/;
 
 export const authorValidationMessage = '작성자 이름은 한글, 영어, 숫자 조합 2자 이상 255자 이하로 입력해주세요.';
 export const commentContentValidationMessage = '댓글은 한글, 영어, 숫자와 일반적인 특수문자만 사용하여 2자 이상 500자 이하로 입력해주세요.';
