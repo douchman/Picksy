3bd767d67337b5c97192f22f83b3b48cab53fa8d
buck <take6948@naver.com>
2025-04-25 12:29:51 +0900

    :sparkles: 대결주제 및 엔트리 식별자 저장 #187

---
+++ src/main/resources/static/statistics/entry/js/entry-versus-statistics.js
@@ -0,0 +1,19 @@
+import { targetTopic, targetEntry } from "./const";
+
+document.addEventListener('DOMContentLoaded', () =>{
+    if(saveTargetTopicIdAndEntryId()){
+
+    }
+});
+
+/* 대결주제 및 엔트리 식별자 추출 & 저장 */
+function saveTargetTopicIdAndEntryId(){
+    const match = window.location.pathname.match(/\/topic\/(\d+)\/entry\/(\d+)/);
+
+    if( match ) {
+        targetTopic.id = match[1];
+        targetEntry.id = match[2];
+    }
+
+    return false;
+}
