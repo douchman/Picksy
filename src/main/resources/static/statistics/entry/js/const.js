409d46f1a8455dcac13e083c7dd165c84b86c1c2
buck <take6948@naver.com>
2025-04-25 12:29:34 +0900

    :sparkles: 대결주제 및 엔트리 const 추가 #187

---
+++ src/main/resources/static/statistics/entry/js/const.js
@@ -0,0 +1,11 @@
+export const targetTopic = {
+    id : null,
+    title : null,
+    description : null,
+}
+
+export const targetEntry = {
+    id : null,
+    name : null,
+    description: null,
+}
