1c045f6b3607dfbef68f1f3bd187a926da818abc
buck <take6948@naver.com>
2025-04-28 16:18:37 +0900

    :wrench: 통합 404 페이지 이동을 위한 config 추가 #178

---
+++ src/main/java/com/buck/vstournament/config/ErrorPageConfig.java
@@ -0,0 +1,15 @@
+package com.buck.vstournament.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class ErrorPageConfig implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/page-not-found"));
+    }
+}
