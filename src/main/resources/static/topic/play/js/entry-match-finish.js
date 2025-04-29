4c75f28f6541a5cd98ba5d232b5132b5de9b3df7
buck <take6948@naver.com>
2025-04-29 17:30:52 +0900

    :sparkles: 엔트리 매치 종료 기능 담당 JS 추가 #196

---
+++ src/main/resources/static/topic/play/js/entry-match-finish.js
@@ -0,0 +1,38 @@
+import {setupMatchStageComment} from "./match-stage-comment/match-stage-comment.js";
+
+// 매치 종료
+export function finishEntryMatch(){
+    setTimeout(() =>{
+        removeLoserEntrySlot(); // 패배한 엔트리 슬롯 제거
+        setupWinnerEntry(); // 승리한 엔트리 셋업
+        setupMatchStageComment(); // 대결주제 유저 코멘트 랜더링
+    }, 2000);
+
+}
+
+/**
+ * 승리한 엔트리 셋업
+ * - winner class 제거 -> 스타일 조정
+ * - 불필요한 UI 제거
+ * - 승리 엔트리 인덱스 랜더링
+ */
+function setupWinnerEntry(){
+    const winnerEntry = document.querySelector('.entry-slot.winner');
+    winnerEntry.classList.remove('winner');
+    winnerEntry.querySelector('.btn-select-entry').remove();
+
+    renderWinnerEntryIndex(winnerEntry);
+}
+
+// 승리 엔트리 인덱스 랜더링
+function renderWinnerEntryIndex(winnerEntry){
+    const winnerEntryIndex =
+        `<p class="winner-entry-index">&lt;우승 엔트리&gt;</p>`;
+
+    winnerEntry.insertAdjacentHTML('afterbegin', winnerEntryIndex);
+}
+
+// 패배한 엔트리 제거
+function removeLoserEntrySlot(){
+    document.querySelector('.entry-slot.loser').remove();
+}
