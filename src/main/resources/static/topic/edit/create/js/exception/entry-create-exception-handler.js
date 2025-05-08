cd8eaa95c5d9fe1bbc3abce4744686474bd2926e
buck <take6948@naver.com>
2025-05-08 13:49:16 +0900

    :sparkles: 엔트리 관련 exception class 및 핸들러 추가 #227

---
+++ src/main/resources/static/topic/edit/create/js/exception/entry-create-exception-handler.js
@@ -0,0 +1,19 @@
+import {GlobalExceptionHandler} from "../../../../../global/exception/global-exception-handler.js";
+import {showToastMessage} from "../../../../../global/popup/js/common-toast-message.js";
+import {EntryCreateException} from "../../../core/js/exception/EntryEditException.js";
+
+export class EntryCreateExceptionHandler extends GlobalExceptionHandler{
+    handle(error){
+        if( error instanceof EntryCreateException) {
+            this.handleEntryCreateException(error);
+        }  else{
+            super.handle(error);
+        }
+    }
+
+    handleEntryCreateException(error){
+        console.error('[Topic Create Exception]' , error);
+        showToastMessage(`${error.message}`, 'error', 2500);
+    }
+
+}
