3292e28921e133cec570ff608254b6d381b5b93a
buck <take6948@naver.com>
2025-05-09 16:43:56 +0900

    :sparkles: 대결주제 식별자 정보 저장 #216

--- src/main/resources/static/topic/edit/modify/js/modify-main.js
+++ src/main/resources/static/topic/edit/modify/js/modify-main.js
@@ -2,13 +2,32 @@ import {setupTopicSection, modifyTopic, registerTopic} from "./topic-section.js"
 import {setupEntrySection, registerEntries} from "./entry-section.js";
 import {showToastMessage} from "../../../../global/popup/js/common-toast-message.js";
 import {createdTopic} from "../../core/js/const/const.js";
+import {TopicEditExceptionHandler} from "../../core/js/exception/topic-edit-exception-handler.js";
+import {TopicIdSaveException} from "../../core/js/exception/TopicEditException.js";
 
-document.addEventListener('DOMContentLoaded', () => {
-    setupTopicSection();
-    setupEntrySection();
-    addBottomBtnGroupEvents();
+const topicEditExceptionHandler = new TopicEditExceptionHandler();
+
+document.addEventListener('DOMContentLoaded', async () => {
+    if(saveTopicId()){
+        setupTopicSection();
+        setupEntrySection();
+        addBottomBtnGroupEvents();
+    }
 });
 
+function saveTopicId(){
+    const path = window.location.pathname;
+    const segments = path.split('/');
+    const topicId = Number(segments[segments.length - 1]);
+
+    if ( !Number.isFinite(topicId) ){
+        topicEditExceptionHandler.handle(new TopicIdSaveException());
+        return false;
+    }
+
+    createdTopic.setId(topicId)
+    return true;
+}
 
 function addBottomBtnGroupEvents(){
     document.querySelector('#btn-save').addEventListener('click' , async () => {
