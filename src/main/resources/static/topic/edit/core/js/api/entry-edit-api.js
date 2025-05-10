15e6460fa4c201a72ae91c103a62cb37af590010
buck <take6948@naver.com>
2025-05-10 16:00:12 +0900

    :sparkles: 생성되어있는 엔트리 조회 api 추가 #216

--- src/main/resources/static/topic/edit/core/js/api/entry-edit-api.js
+++ src/main/resources/static/topic/edit/core/js/api/entry-edit-api.js
@@ -17,3 +17,19 @@ export async function createEntries(topicId, requestBody){
 async function fetchCreateEntries(topicId, requestBody){
     return await apiRequest(`topics/${createdTopic.getId()}/entries`, ApiMethod.POST, requestBody, true);
 }
+
+// 엔트리 조회
+export async function getEntryList(topicId){
+    const { status, data : entryListResult, isAuthOrNetworkException } = await fetchEntryList(topicId);
+
+    if( isAuthOrNetworkException ) return null;
+
+    if(status === 200 && entryListResult ){ return entryListResult; }
+
+    return null;
+}
+
+// 엔트리 리스트 조회 API fetch
+async function fetchEntryList(topicId){
+    return await apiRequest(`topics/${topicId}/entries`, ApiMethod.GET,{}, false);
+}
