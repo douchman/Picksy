88947479d61562eac93ddb25ef899cc1379de2ab
buck <take6948@naver.com>
2025-05-10 17:12:34 +0900

    :sparkles: 기존 엔트리 조회 데이터를 다루는 js 추가 #216

---
+++ src/main/resources/static/topic/edit/core/js/const/initial-entry-map.js
@@ -0,0 +1,32 @@
+export const initialEntryDataMap = new Map();
+
+export function appendToInitialEntryDataMap(entry){
+    const entryName = entry.entryName;
+    const description = entry.description;
+    const mediaType = entry.mediaType;
+    const mediaUrl = entry.mediaUrl;
+    const thumbnail = entry.thumbnail;
+    const isMediaChanged = false;
+
+    initialEntryDataMap.set(entry.id ,{
+            entryName,
+            description,
+            mediaType,
+            mediaUrl,
+            thumbnail,
+            isMediaChanged
+        });
+}
+
+export function isModifiedEntry(entryId, currentData){
+    const initialEntryData = initialEntryDataMap.get(entryId);
+
+    if( !initialEntryData) return true;
+
+    return (currentData.entryName !== initialEntryData.entryName ||
+        currentData.description !== initialEntryData.description ||
+        currentData.mediaType !== initialEntryData.mediaType ||
+        currentData.mediaUrl !== initialEntryData.mediaUrl ||
+        currentData.thumbnail !== initialEntryData.thumbnail ||
+        currentData.isMediaChanged);
+}
