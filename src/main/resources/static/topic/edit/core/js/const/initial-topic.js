82f81e65148b1ce52ed6705bb783503f990c6d34
buck <take6948@naver.com>
2025-05-13 19:04:48 +0900

    :sparkles: 대결주제 기존 데이터 비교 함수 추가 #234

--- src/main/resources/static/topic/edit/core/js/const/initial-topic.js
+++ src/main/resources/static/topic/edit/core/js/const/initial-topic.js
@@ -8,3 +8,11 @@ export function setInitialTopic(topic){
         // TODO : visibility 관련 API 수정 이후 추가 필요
     }
 }
+
+export function isModifiedTopic(currentData){
+    return (
+        initialTopic.title !== currentData.title ||
+        initialTopic.subject !== currentData.subject ||
+        initialTopic.description !== currentData.description
+    );
+}
