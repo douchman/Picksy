889ae4ce34f87678e40fbcf81d9b118edce8ea37
buck <take6948@naver.com>
2025-05-13 14:24:24 +0900

    :bug: 누락된 예외 핸들러 추가 #231

--- src/main/resources/static/topic/edit/core/js/topic-section/topic-actions.js
+++ src/main/resources/static/topic/edit/core/js/topic-section/topic-actions.js
@@ -2,6 +2,9 @@ import {buildValidatedTopicRegisterFormData} from "./topic-form-data-builder.js"
 import {createTopic, updateTopic} from "../api/topic-edit-api.js";
 import {createdTopic} from "../const/const.js";
 import {TopicCreateException, TopicUpdateException} from "../exception/TopicEditException.js";
+import {TopicEditExceptionHandler} from "../exception/topic-edit-exception-handler.js";
+
+const topicEditExceptionHandler = new TopicEditExceptionHandler();
 
 export async function registerTopic(){
     const {validationResult , formData : {topicTitle, topicSubject, topicDesc, topicThumb, visibility }} = buildValidatedTopicRegisterFormData();
