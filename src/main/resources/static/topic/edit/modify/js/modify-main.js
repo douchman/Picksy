6594a532e11f013ed69f9d86f8a481f0e968f111
buck <take6948@naver.com>
2025-05-13 13:31:13 +0900

    :recycle: 엔트리 등록/수정 함수 actions 로부터 import #231

--- src/main/resources/static/topic/edit/modify/js/modify-main.js
+++ src/main/resources/static/topic/edit/modify/js/modify-main.js
@@ -1,10 +1,11 @@
 import {setupTopicSection} from "./topic-section.js";
-import {setupEntrySection, registerEntries, updateEntries} from "./entry-section.js";
+import {setupEntrySection} from "./entry-section.js";
 import {showToastMessage} from "../../../../global/popup/js/common-toast-message.js";
 import {createdTopic} from "../../core/js/const/const.js";
 import {TopicEditExceptionHandler} from "../../core/js/exception/topic-edit-exception-handler.js";
 import {TopicIdSaveException} from "../../core/js/exception/TopicEditException.js";
 import {modifyTopic, registerTopic} from "../../core/js/topic-section/topic-actions.js";
+import {registerEntries, updateEntries} from "../../core/js/entry-section/entry-actions.js";
 
 const topicEditExceptionHandler = new TopicEditExceptionHandler();
 
