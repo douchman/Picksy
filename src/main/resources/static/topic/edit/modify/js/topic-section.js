0cf6bb65f4c7d4b1eb692358093055da33406d4d
buck <take6948@naver.com>
2025-05-13 19:00:34 +0900

    :recycle: 대결주제 랜더링 함수 수정 및 셋업 함수에서 호출 #234

--- src/main/resources/static/topic/edit/modify/js/topic-section.js
+++ src/main/resources/static/topic/edit/modify/js/topic-section.js
@@ -1,9 +1,11 @@
 import {addTopicSectionEvents} from "../../core/js/topic-section/section-events.js";
 import {getExistTopicDetail} from "../../core/js/topic-section/topic-actions.js";
+import {renderTopicDetail} from "../../core/js/topic-section/topic-renderer";
 
 export async function setupTopicSection() {
-    const topicDetail = await getExistTopicDetail();
-    if( topicDetail ) {
+    const topicDetailResult = await getExistTopicDetail();
+    if( topicDetailResult ) {
+        renderTopicDetail(topicDetailResult)
         addTopicSectionEvents();
     }
 }
