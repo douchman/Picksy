1b37670fc6929c64f362aaa55658c90bb5d1ba47
buck <take6948@naver.com>
2025-05-13 19:03:34 +0900

    :sparkles: 대결주제 초기 값 관련 함수 추가 및 함수 실행 #234

---
+++ src/main/resources/static/topic/edit/core/js/const/initial-topic.js
@@ -0,0 +1,10 @@
+export const initialTopic = {}
+
+export function setInitialTopic(topic){
+    if(topic){
+        initialTopic.title = topic.title;
+        initialTopic.subject = topic.subject;
+        initialTopic.description = topic.description;
+        // TODO : visibility 관련 API 수정 이후 추가 필요
+    }
+}
--- src/main/resources/static/topic/edit/modify/js/topic-section.js
+++ src/main/resources/static/topic/edit/modify/js/topic-section.js
@@ -1,11 +1,17 @@
 import {addTopicSectionEvents} from "../../core/js/topic-section/section-events.js";
 import {getExistTopicDetail} from "../../core/js/topic-section/topic-actions.js";
-import {renderTopicDetail} from "../../core/js/topic-section/topic-renderer";
+import {renderTopicDetail} from "../../core/js/topic-section/topic-renderer.js";
+import {setInitialTopic} from "../../core/js/const/initial-topic.js";
 
 export async function setupTopicSection() {
     const topicDetailResult = await getExistTopicDetail();
     if( topicDetailResult ) {
-        renderTopicDetail(topicDetailResult)
+        cacheInitialTopicData(topicDetailResult.topic);
+        renderTopicDetail(topicDetailResult);
         addTopicSectionEvents();
     }
+}
+
+function cacheInitialTopicData(topic){
+    setInitialTopic(topic);
 }
