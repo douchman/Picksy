90cc1f6a6bdc66304a2c8650cde71e987ff5df66
buck <take6948@naver.com>
2025-05-19 13:55:23 +0900

    :recycle: 대결주제 카드 클릭 이벤트 처리방식 수정 #246

--- src/main/resources/static/topic/my/js/topic-cards/topic-cards.js
+++ src/main/resources/static/topic/my/js/topic-cards/topic-cards.js
@@ -1,22 +1,35 @@
 import {setupInfiniteScrollObserver} from "./topic-cards-scroll-observer.js";
 
+const ACTION_HANDLERS = {
+    modify : moveToTopicModifyPage
+}
+
 export function setupTopicCards(){
     setupInfiniteScrollObserver();
     setupTopicCardEvents();
 }
 
 function setupTopicCardEvents(){
-    // 대결주제 카드 -> 수정 버튼 이벤트
+    addTopicCardClickEvent();
+}
+
+// 대결주제 카드 클릭 -> 요소에 따라 이벤트 위임
+function addTopicCardClickEvent(){
     document.querySelector('#topic-cards').addEventListener('click', (e) => {
-        const modifyButton = e.target.closest('.btn-modify');
-        if(modifyButton ){
-            const topicCard = modifyButton.closest('.topic-card');
-            moveToTopicModifyPage(topicCard)    ;
+        const actionTarget = e.target.closest('[data-action]');
+        if(!actionTarget) return;
+
+        const action = actionTarget.dataset.action;
+        const handler = ACTION_HANDLERS[action];
+
+        if( handler ) {
+            const topicCard = actionTarget.closest('.topic-card');
+            const topicId = topicCard?.id;
+            if( topicId ) { handler(topicId); }
         }
     });
 }
 
-function moveToTopicModifyPage(topicCard){
-    const topicId = topicCard.id;
+function moveToTopicModifyPage(topicId){
     window.open(`/topic/modify/${topicId}`, '_blank'); // 새 탭으로 수정페이지 열기
 }
