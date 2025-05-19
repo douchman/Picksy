24cbc59288045ff82f5be41bab0d77f77a8a4ba1
buck <take6948@naver.com>
2025-05-19 13:38:07 +0900

    :sparkles: 주제 카드 수정 버튼 이벤트 추가 #246

--- src/main/resources/static/topic/my/js/topic-cards/topic-cards.js
+++ src/main/resources/static/topic/my/js/topic-cards/topic-cards.js
@@ -2,4 +2,21 @@ import {setupInfiniteScrollObserver} from "./topic-cards-scroll-observer.js";
 
 export function setupTopicCards(){
     setupInfiniteScrollObserver();
+    setupTopicCardEvents();
+}
+
+function setupTopicCardEvents(){
+    // 대결주제 카드 -> 수정 버튼 이벤트
+    document.querySelector('#topic-cards').addEventListener('click', (e) => {
+        const modifyButton = e.target.closest('.btn-modify');
+        if(modifyButton ){
+            const topicCard = modifyButton.closest('.topic-card');
+            moveToTopicModifyPage(topicCard)    ;
+        }
+    });
+}
+
+function moveToTopicModifyPage(topicCard){
+    const topicId = topicCard.id;
+    window.open(`/topic/modify/${topicId}`, '_blank'); // 새 탭으로 수정페이지 열기
 }
