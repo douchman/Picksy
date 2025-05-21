e6b475be44391b90a17c8e8c4c059efbbb451eb3
buck <take6948@naver.com>
2025-05-21 16:54:54 +0900

    :recycle: 내 대결주제 페이지 내 토너먼트 선택 대화상자 사용하지 않음 #261 - 스타일 제거

--- src/main/resources/static/topic/my/js/topic-cards/topic-cards.js
+++ src/main/resources/static/topic/my/js/topic-cards/topic-cards.js
@@ -1,8 +1,4 @@
 import {setupInfiniteScrollObserver} from "./topic-cards-scroll-observer.js";
-import {
-    openTournamentSelectDialog,
-    setupTournamentSelectDialog
-} from "../../../../global/tounament-select-dialog/js/tournament-select-dialog.js";
 
 const ACTION_HANDLERS = {
     play : openTournamentSelect,
@@ -13,7 +9,6 @@ const ACTION_HANDLERS = {
 export function setupTopicCards(){
     setupInfiniteScrollObserver();
     setupTopicCardEvents();
-    setupTournamentSelectDialog();
 }
 
 function setupTopicCardEvents(){
@@ -38,7 +33,7 @@ function addTopicCardClickEvent(){
 }
 
 async function openTournamentSelect(topicId){
-    await openTournamentSelectDialog(topicId);
+    // TODO : start topic
 }
 
 function moveToTopicModifyPage(topicId){
