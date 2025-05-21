11a29b0f671b7c23bc2f804497181872798303f7
buck <take6948@naver.com>
2025-05-21 17:06:21 +0900

    :recycle: 토너먼트 선택 대화상자 setup 및 대화상자 open #261

--- src/main/resources/static/topic/play/js/topic-play.js
+++ src/main/resources/static/topic/play/js/topic-play.js
@@ -1,32 +1,32 @@
-import {topic, playRecord} from "./const.js";
-import {loadEntryMatchInfo} from "./entry-match.js";
+import {topic} from "./const.js";
 import {loadYoutubeIframeAPI, onYouTubeIframeApiReady} from "../../../global/js/youtube-iframe-api.js";
 import {getTopicDetail} from "./topic-play-api.js";
 import {TopicPlayExceptionHandler} from "./exception/topic-play-exception-handler.js";
-import {SavePlayRecordInfoException, SetTopicInfoException} from "./exception/TopicPlayException.js";
+import { SetTopicInfoException} from "./exception/TopicPlayException.js";
+import {
+    openTournamentSelectDialog,
+    setupTournamentSelectDialog
+} from "../tounament-select-dialog/js/tournament-select-dialog.js";
 
 const topicPlayExceptionHandler = new TopicPlayExceptionHandler();
 
 document.addEventListener('DOMContentLoaded', async () => {
 
     const topicSuccess  = await saveTopicInfo(); // 대결주제 식별자 변수화
-    const playRecordSuccess = savePlayRecordInfo(); // 대결진행기록 식별자 변수화
 
-    if( topicSuccess && playRecordSuccess){
+    if( topicSuccess ){
+        setupTournamentSelectDialog();
         loadYoutubeIframeAPI();
     }
 
     // 유튜브 API 로드 후 호출되는 전역 함수
     onYouTubeIframeApiReady(async () =>{
-        if( topicSuccess && playRecordSuccess){
-            addTopicPlayEvents();
-            await loadEntryMatchInfo();
-        }
+        await openTournamentSelectDialog(topic.getId());
+        addTopicPlayEvents();
     });
 });
 
 function addTopicPlayEvents(){
-
     document.querySelector('#btn-back').addEventListener('click', () =>{
         location.href = '/';
     });
@@ -46,20 +46,6 @@ async function saveTopicInfo(){
     } else {
         topicPlayExceptionHandler.handle(new SetTopicInfoException(topicDetailResult.message));
         return false;
-    }
-
-    return true;
-}
-
-function savePlayRecordInfo(){
-    const storedPlayRecordIdName = `topic-${topic.getId()}-playRecord-id`;
-
-    const playRecordId = localStorage.getItem(storedPlayRecordIdName);
-
-    if( !playRecordId ){
-        topicPlayExceptionHandler.handle(new SavePlayRecordInfoException())
-    } else {
-        playRecord.setId(playRecordId);
     }
 
     return true;
