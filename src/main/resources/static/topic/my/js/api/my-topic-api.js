ec04bacd538069ee1d31e978949485db2bfc2d78
buck <take6948@naver.com>
2025-05-26 20:02:21 +0900

    :recycle: 내가 생성한 대결주제 조회 api 구조 변경 #281

--- src/main/resources/static/topic/my/js/api/my-topic-api.js
+++ src/main/resources/static/topic/my/js/api/my-topic-api.js
@@ -3,16 +3,5 @@ import {ApiMethod} from "../../../../global/api/api-method.js";
 
 // 내가 생성한 대결주제 조회
 export async function getMyTopic(requestBody){
-    const { status, data : myTopicResult, isAuthOrNetworkException } = await fetchMyTopic(requestBody);
-
-    if( isAuthOrNetworkException)  return null;
-
-    if( status === 200 && myTopicResult ) return myTopicResult;
-
-    return null;
-}
-
-// 내가 생성한 대결주제 조회 API fetch
-async function fetchMyTopic(requestBody){
     return await apiRequest('my/topics' , ApiMethod.GET, requestBody, false);
 }
