20092df647845f7e4a31ad8f76e330ede77ce5ff
buck <take6948@naver.com>
2025-05-26 18:02:07 +0900

    :recycle: 홈 내 대결주제 조회 api 호출 함수 단순화 #273

--- src/main/resources/static/home/js/home-api.js
+++ src/main/resources/static/home/js/home-api.js
@@ -3,16 +3,5 @@ import {ApiMethod} from "../../global/api/api-method.js";
 
 // 대결 주제 검색
 export async function searchTopics(requestParams){
-    const { status, data : topicSearchResult, isAuthOrNetworkException} = await fetchTopicsSearch(requestParams);
-
-    if(isAuthOrNetworkException) return null;
-
-    if( status === 200 && topicSearchResult ){ return topicSearchResult }
-
-    return null;
-}
-
-// 대결 주제 검색 API fetch
-async function fetchTopicsSearch(requestParams){
     return await apiRequest('topics', ApiMethod.GET, requestParams);
 }
