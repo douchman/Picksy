58dca4c81bbcadb368fcb1d659646d29721252b2
buck <take6948@naver.com>
2025-05-26 19:33:04 +0900

    :recycle: 대결 주제 관련 action 함수 처리 구조 및 예외 처리 구조 변경 #280

--- src/main/resources/static/topic/edit/core/js/exception/topic-edit-exception-handler.js
+++ src/main/resources/static/topic/edit/core/js/exception/topic-edit-exception-handler.js
@@ -1,14 +1,19 @@
 import {GlobalExceptionHandler} from "../../../../../global/exception/global-exception-handler.js";
-import {TopicCreateException, TopicIdSaveException, TopicUpdateException} from "./TopicEditException.js";
+import {TopicIdSaveException} from "./TopicEditException.js";
 import {showToastMessage} from "../../../../../global/popup/js/common-toast-message.js";
+import {ApiResponseException} from "../../../../../global/api/exception/ApiException.js";
 
 export class TopicEditExceptionHandler extends GlobalExceptionHandler{
-    handle(error){
-        if( error instanceof TopicCreateException) {
-            this.handleTopicCreateException(error);
-        } else if ( error instanceof TopicUpdateException ) {
-            this.handleTopicUpdateException(error);
-        } else if ( error instanceof TopicIdSaveException ) {
+    handle(error, {context}){
+        if( error instanceof ApiResponseException) {
+            if( context === 'topicCreate')
+                this.handleTopicCreateException(error);
+            else if ( context === 'topicModify')
+                this.handleTopicUpdateException(error);
+            else if( context === 'topicDetail')
+                this.handleTopicDetailException(error);
+        }
+        else if ( error instanceof TopicIdSaveException ) {
             this.handleTopicIdSaveException(error);
         } else{
             super.handle(error)
@@ -23,6 +28,14 @@ export class TopicEditExceptionHandler extends GlobalExceptionHandler{
     handleTopicUpdateException(error){
         console.error('[Topic Update Exception]' , error);
         showToastMessage(`${error.message}`, 'error', 2500);
+    }
+
+    handleTopicDetailException(error){
+        console.error('[Topic Detail Exception]' , error);
+        showToastMessage(`${error.message}`, 'error', 2500);
+        setTimeout(() => {
+            location.href = '/';
+        }, 3000)
     }
 
     handleTopicIdSaveException(error){
