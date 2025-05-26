1a26e47a1b489266def9561cec2995f8f2e39990
buck <take6948@naver.com>
2025-05-26 16:57:31 +0900

    :truck: 유저 인증 관련 오류를 globalException 으로 이동 #274

--- src/main/resources/static/global/api/exception/ApiException.js
+++ src/main/resources/static/global/api/exception/ApiException.js
@@ -1,11 +1,3 @@
-export class UserAuthException extends Error {
-    constructor(message = 'User Auth Exception', status = 401) {
-        super();
-        this.status = status;
-        this.name = 'UserAuthException';
-    }
-}
-
 export class FetchNetworkException extends Error {
     constructor(message = 'API Network Exception', status = 500) {
         super(message);
--- src/main/resources/static/global/api/exception/api-exception-handler.js
+++ src/main/resources/static/global/api/exception/api-exception-handler.js
@@ -1,34 +1,15 @@
 import {GlobalExceptionHandler} from "../../exception/global-exception-handler.js";
 import {renderCommonAlertMessage} from "../../alert/js/common-alert-message.js";
-import {FetchNetworkException, UserAuthException} from "./ApiException.js";
+import {FetchNetworkException} from "./ApiException.js";
 
 export class ApiExceptionHandler extends GlobalExceptionHandler{
     handle(error){
         if( error instanceof FetchNetworkException ){
             this.handleApiNetworkException(error);
         }
-        else if ( error instanceof UserAuthException ){
-            this.handleUserAuthException(error);
-        }
         else{
             super.handle(error)
         }
-    }
-
-    handleUserAuthException(error){
-        console.log('[User Auth Exception]', error);
-        if( error.status === 401 ){
-            renderCommonAlertMessage(
-                '회원 이용 기능',
-                '로그인이 필요한 기능입니다.',
-                () => {location.href ='/login'});
-        } else if( error.status === 403 ){
-            renderCommonAlertMessage(
-                '권한 없음',
-                '이용권한이 없습니다.',
-                () => {location.href ='/'});
-        }
-
     }
 
     handleApiNetworkException(error){
