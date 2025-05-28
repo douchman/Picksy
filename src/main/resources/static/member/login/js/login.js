25a07f811b09b16ee1fc07df08e529f8cd40da01
buck <take6948@naver.com>
2025-05-28 16:14:28 +0900

    :recycle: setup 함수 및 이벤트 등록 함수로 분리 #296

--- src/main/resources/static/member/login/js/login.js
+++ src/main/resources/static/member/login/js/login.js
@@ -4,18 +4,28 @@ import {LoginExceptionHandler} from "./exception/login-exception-handler.js";
 const loginExceptionHandler = new LoginExceptionHandler();
 
 document.addEventListener("DOMContentLoaded", async () => {
+    setupLoginPage();
+});
 
+function setupLoginPage(){
+    addLoginPageEvents();
+}
+
+function addLoginPageEvents(){
+    // 로그인 버튼 이벤트
     document.querySelector('#btn-login').addEventListener('click', login);
+
+    // 계정생성 버튼 이벤트
     document.querySelector('#btn-register').addEventListener('click', moveToRegisterPage);
 
-    document.querySelector('#password').addEventListener('keydown', function(event){
+    // 패스워드 입력란 key down 이벤트
+    document.querySelector('#password').addEventListener('keydown', async function(event){
         const keyEvent = event.key;
         if( keyEvent === 'Enter'){
-            login();
+            await login();
         }
     });
-
-});
+}
 
 async function login(){
     const id = document.querySelector('#id').value;
