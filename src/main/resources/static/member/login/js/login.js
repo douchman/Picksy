2936419fe11a77342d74c136553d1bb5b7f4f87e
buck <take6948@naver.com>
2025-05-28 16:12:13 +0900

    :recycle: 클릭 이벤트 함수 분리 #296

--- src/main/resources/static/member/login/js/login.js
+++ src/main/resources/static/member/login/js/login.js
@@ -5,36 +5,38 @@ const loginExceptionHandler = new LoginExceptionHandler();
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    document.querySelector('#btn-login').addEventListener('click', async function(){
-        const id = document.querySelector('#id').value;
-        const password = document.querySelector('#password').value;
-
-        if( validateLoginForm(id, password)){
-            let loginResult;
-            try{
-                loginResult = await memberLogin({id, password});
-                if(loginResult){
-                    location.href = '/';
-                }
-            }catch(error){
-                loginExceptionHandler.handle(error, {context : 'userLogin'});
-            }
-        }
-    });
-
-    document.querySelector('#btn-register').addEventListener('click', () =>{
-       location.href='/register';
-    });
+    document.querySelector('#btn-login').addEventListener('click', login);
+    document.querySelector('#btn-register').addEventListener('click', moveToRegisterPage);
 
     document.querySelector('#password').addEventListener('keydown', function(event){
         const keyEvent = event.key;
-
         if( keyEvent === 'Enter'){
-            document.querySelector('#btn-login').click();
+            login();
         }
     });
 
 });
+
+async function login(){
+    const id = document.querySelector('#id').value;
+    const password = document.querySelector('#password').value;
+
+    if( validateLoginForm(id, password)){
+        let loginResult;
+        try{
+            loginResult = await memberLogin({id, password});
+            if(loginResult){
+                location.href = '/';
+            }
+        }catch(error){
+            loginExceptionHandler.handle(error, {context : 'userLogin'});
+        }
+    }
+}
+
+function moveToRegisterPage(){
+    location.href='/register';
+}
 
 function validateLoginForm(id, password){
     if( !id || id === ''){
