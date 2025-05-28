21e28e732de585ca35ffeb2fcbc541f7bb0c3b09
buck <take6948@naver.com>
2025-05-28 13:52:19 +0900

    :sparkles: 상대한 엔트리 통계 api 분리 #290

--- src/main/resources/static/statistics/entry-versus/js/versus-statistics-api.js
+++ src/main/resources/static/statistics/entry-versus/js/versus-statistics-api.js
@@ -4,3 +4,7 @@ import {ApiMethod} from "../../../global/api/api-method.js";
 export async function getTargetEntryStatistics(topicId, entryId){
     return await apiRequest(`statistics/topics/${topicId}/entries/${entryId}`, ApiMethod.GET, {}, false);
 }
+
+export async function getEntryVersusStatistics(topicId, entryId){
+    return await apiRequest(`statistics/versus/topics/${topicId}/entries/${entryId}`, ApiMethod.GET, {}, false);
+}
