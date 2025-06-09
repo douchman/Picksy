09fca56a5768c17b249fca157cf0921f0a6fe5b0
buck <take6948@naver.com>
2025-06-09 13:28:13 +0900

    :fire: 엔트리 통계 예외 처리 핸들러 오류로깅 제거 #341

--- src/main/resources/static/statistics/topic-entry-stats/js/entry-statistics-table/exception/entry-statistics-exception-handler.js
+++ src/main/resources/static/statistics/topic-entry-stats/js/entry-statistics-table/exception/entry-statistics-exception-handler.js
@@ -13,8 +13,7 @@ export class EntryStatisticsExceptionHandler extends GlobalExceptionHandler {
         }
     }
 
-    handleGetEntryStatisticsException(error) {
-        console.error('[Get Entry Statistics Exception]', error);
+    handleGetEntryStatisticsException() {
         showToastMessage('엔트리 통계정보를 조회할 수 없어요. 새로고침 또는 탭을 닫고 다시 시도해 주세요', 'error', 3000);
     }
 }
