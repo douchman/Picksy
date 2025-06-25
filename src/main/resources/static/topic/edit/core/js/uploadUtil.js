7861d8dbde810cefab3687df6777f524bf915983
buck <take6948@naver.com>
2025-06-25 19:32:59 +0900

    :sparkles: 다중 파일 s3 업로드 추가 #352

--- src/main/resources/static/topic/edit/core/js/uploadUtil.js
+++ src/main/resources/static/topic/edit/core/js/uploadUtil.js
@@ -15,3 +15,29 @@ export async function uploadSingleFileToS3(presignedUrl, file){
         throw Error(error);
     }
 }
+
+export async function uploadMultipleFilesToS3(uploadFileList){
+    try{
+
+        const uploadResult = await Promise.allSettled(
+            uploadFileList.map(uploadFile =>
+            fetch(uploadFile.presignedUploadUrl, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': uploadFile.file.type
+                },
+                body: uploadFile.file
+            }))
+        );
+
+        return isAllUploadSuccessful(uploadResult);
+
+    }catch(error){
+        throw new Error(error.message || String(error));
+    }
+}
+
+function isAllUploadSuccessful(uploadResult){
+    const rejected = uploadResult.filter(r => r.status === 'rejected');
+    return rejected.length <= 0;
+}
