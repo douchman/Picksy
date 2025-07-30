f29dcca9f81b8fe81a7f58c934475600f95c9565
buck <take6948@naver.com>
2025-07-30 19:15:01 +0900

    :sparkles: 피드 유형 필터 선택기 기능 구현 #434

---
+++ src/main/resources/static/notice/js/notice-filter.js
@@ -0,0 +1,40 @@
+import {noticeSearchParams} from "./notice-const.js";
+import {removeAllNoticeCards} from "./notice-render.js";
+import {startScrollObserver} from "./notice-scroll-observer.js";
+
+export function setupNoticeFilter(){
+    setupNoticeTypeSelect();
+}
+
+function setupNoticeTypeSelect(){
+
+    // 필터 선택 open
+    window.addEventListener('click', function(e){
+       const hasNoticeTypeFilterClass = ['selected-notice-type', 'notice-type-select-items'].some(cls => e.target.classList.contains(cls));
+        toggleNoticeTypeFilterActive(hasNoticeTypeFilterClass);
+    });
+
+    document.querySelector('#notice-type-select-items').addEventListener('click', function(e){
+        const noticeTypeSelectItem = e.target.closest('.notice-type-select-item');
+        if(noticeTypeSelectItem) noticeTypeItemSelected(noticeTypeSelectItem);
+    });
+}
+
+function noticeTypeItemSelected(selectedItem){
+    const noticeType = selectedItem.value;
+    const noticeTypeName = selectedItem.innerText;
+    const selectedNoticeTypeEle = document.querySelector('#selected-notice-type');
+
+    selectedNoticeTypeEle.innerText = noticeTypeName;
+
+    noticeSearchParams.initPage(); // 현재 페이지 값 리셋
+    noticeSearchParams.noticeType = noticeType;
+    removeAllNoticeCards(); // 랜더링 된 공지사항 카드 모두 제거
+    startScrollObserver(); // 옵저버 재시작
+
+    toggleNoticeTypeFilterActive(false);
+}
+
+function toggleNoticeTypeFilterActive(isActive){
+    document.querySelector('#notice-type-select').classList.toggle('active', isActive);
+}
