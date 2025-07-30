d67b6c6e9db9ef3c10386e62af04948318a46dbc
buck <take6948@naver.com>
2025-07-30 16:15:28 +0900

    :recycle: 공지사항 카드 관련 함수 분리 #433 - notice-cards.js 추가

---
+++ src/main/resources/static/notice/js/notice-cards.js
@@ -0,0 +1,30 @@
+export function setupNoticeCards(){
+    addNoticeCardEvents();
+}
+
+function addNoticeCardEvents(){
+    document.querySelector('#notice-cards').addEventListener('click', function(event){
+        const noticeCard = event.target.closest('.notice-card');
+        if(noticeCard){
+            toggleNoticeCardOpen(noticeCard);
+        }
+    });
+}
+
+// 카드 토글하여 오픈
+// 부드러운 애니메이션을 위해 reflow 로 처리
+function toggleNoticeCardOpen(noticeCard){
+
+    const content = noticeCard.querySelector('.content');
+
+    if (noticeCard.classList.contains('open')) {
+        content.style.maxHeight = content.scrollHeight + 'px';
+        // 강제로 reflow 발생
+        void content.offsetHeight;
+        content.style.maxHeight = '0px';
+        noticeCard.classList.remove('open');
+    } else {
+        content.style.maxHeight = content.scrollHeight + 'px';
+        noticeCard.classList.add('open');
+    }
+}
--- src/main/resources/static/notice/js/notice.js
+++ src/main/resources/static/notice/js/notice.js
@@ -1,32 +1,8 @@
+import {setupNoticeCardsObserver} from "./notice-scroll-observer.js";
+import {setupNoticeCards} from "./notice-cards.js";
+
 document.addEventListener("DOMContentLoaded", () => {
-    addNoticeCardEvents();
+    setupNoticeCards();
+    setupNoticeCardsObserver();
 });
 
-
-function addNoticeCardEvents(){
-    document.querySelector('#notice-cards').addEventListener('click', function(event){
-        const noticeCard = event.target.closest('.notice-card');
-        if(noticeCard){
-            toggleNoticeCardOpen(noticeCard);
-        }
-    });
-
-}
-
-// 카드 토글하여 오픈
-// 부드러운 애니메이션을 위해 reflow 로 처리
-function toggleNoticeCardOpen(noticeCard){
-
-   const content = noticeCard.querySelector('.content');
-
-    if (noticeCard.classList.contains('open')) {
-        content.style.maxHeight = content.scrollHeight + 'px';
-        // 강제로 reflow 발생
-        void content.offsetHeight;
-        content.style.maxHeight = '0px';
-        noticeCard.classList.remove('open');
-    } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-        noticeCard.classList.add('open');
-    }
-}
