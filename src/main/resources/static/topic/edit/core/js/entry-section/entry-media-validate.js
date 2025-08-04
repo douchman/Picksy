d2231ab05a081406637eebb357938b46cde2af8b
buck <take6948@naver.com>
2025-08-04 17:38:18 +0900

    :recycle: 반복적인 경고 알림 방지를 위한 flag 추가 #455 - 기준치가 완화 될 경우 다시 노출 처리

--- src/main/resources/static/topic/edit/core/js/entry-section/entry-media-validate.js
+++ src/main/resources/static/topic/edit/core/js/entry-section/entry-media-validate.js
@@ -2,6 +2,8 @@ import {showToastMessage} from "../../../../../global/toast-message/js/common-to
 import {MediaType} from "../../../../../global/const/const.js";
 import {stagedEntryMedia} from "../staged-entry-media.js";
 
+let hasShownUploadSizeWarning = false;
+
 const MAX_IMAGE_SIZE_MB = 2; // 업로드 가능 비디오 용량 2MB
 const MAX_VIDEO_SIZE_MB = 3; // 업로드 가능 비디오 용량 3MB
 const ALERT_TOTAL_UPLOAD_MB = 40; // 업로드 경고 요량 기준 40MB
@@ -30,6 +32,7 @@ export function validateUploadFile(mediaType, uploadFile){
 }
 
 export function checkTotalUploadSize(){
+    if( hasShownUploadSizeWarning ) return;
     const totalUploadFilesSizeBytes = Object.values(stagedEntryMedia).reduce((totalSize, entry) => {
         const file = entry.media;
         return file instanceof File ? totalSize + file.size : totalSize;
@@ -40,7 +43,10 @@ export function checkTotalUploadSize(){
             toastType : 'alert',
             title : '업로드 용량이 많습니다.',
             content : '등록된 파일의 총 용량이 많아 <b>업로드에 시간이 조금 더 걸릴 수 있어요.</b>',
-            delay: 12000
-        })
+            delay: 20000
+        });
+        hasShownUploadSizeWarning = true;
+    } else{
+        hasShownUploadSizeWarning = false;
     }
 }
