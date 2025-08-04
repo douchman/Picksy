9490bcc48082b01606bb9387d5d90b16ff147851
buck <take6948@naver.com>
2025-08-04 17:26:56 +0900

    :sparkles: 업로드 지연 경고 함수 추가 #455

--- src/main/resources/static/topic/edit/core/js/entry-section/entry-media-validate.js
+++ src/main/resources/static/topic/edit/core/js/entry-section/entry-media-validate.js
@@ -1,10 +1,13 @@
 import {showToastMessage} from "../../../../../global/toast-message/js/common-toast-message.js";
 import {MediaType} from "../../../../../global/const/const.js";
+import {stagedEntryMedia} from "../staged-entry-media.js";
 
 const MAX_IMAGE_SIZE_MB = 2; // 업로드 가능 비디오 용량 2MB
 const MAX_VIDEO_SIZE_MB = 3; // 업로드 가능 비디오 용량 3MB
+const ALERT_TOTAL_UPLOAD_MB = 40; // 업로드 경고 요량 기준 40MB
 const MAX_IMAGE_SIZE_BYTES = MAX_IMAGE_SIZE_MB * 1024 * 1024;
 const MAX_VIDEO_SIZE_BYTES = MAX_VIDEO_SIZE_MB * 1024 * 1024;
+const ALERT_TOTAL_UPLOAD_BYTES = ALERT_TOTAL_UPLOAD_MB * 1024 * 1024;
 
 // 업로드 대상 파일 검사
 export function validateUploadFile(mediaType, uploadFile){
@@ -24,4 +27,20 @@ export function validateUploadFile(mediaType, uploadFile){
         return false;
     }
     return true;
+}
+
+export function checkTotalUploadSize(){
+    const totalUploadFilesSizeBytes = Object.values(stagedEntryMedia).reduce((totalSize, entry) => {
+        const file = entry.media;
+        return file instanceof File ? totalSize + file.size : totalSize;
+    },0); // totalSize 초기값 0
+
+    if(totalUploadFilesSizeBytes > ALERT_TOTAL_UPLOAD_BYTES){
+        showToastMessage({
+            toastType : 'alert',
+            title : '업로드 용량이 많습니다.',
+            content : '등록된 파일의 총 용량이 많아 <b>업로드에 시간이 조금 더 걸릴 수 있어요.</b>',
+            delay: 12000
+        })
+    }
 }
